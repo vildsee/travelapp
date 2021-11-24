81cc16d0f0fa59dba070b7cfa3e2aeecf958ca76
vildsee <78222196+vildsee@users.noreply.github.com>
2021-11-24 17:55:58 +0100

    hotel, flight, packinglist working

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,7 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 import './styles/resets.scss'
+import './styles/extraInfo.scss'
 
 //JavaScript
 import './js/app.js'
@@ -12,8 +13,8 @@ import './js/lists.js'
 //Functions, events
 import { performAction } from './js/app.js'
 import { countdown, countdownConditions, wCountdown, tripLength } from './js/countdown.js'
-import { hotelInfo } from './js/lists.js'
+import { hotelInfo, newElement } from './js/lists.js'
 
 
 
-export { performAction, countdown, countdownConditions, wCountdown, tripLength, hotelInfo }
+export { performAction, countdown, countdownConditions, wCountdown, tripLength, hotelInfo, newElement }
--- src/client/js/lists.js
+++ src/client/js/lists.js
@@ -1,72 +1,108 @@
-//Event for hotel info
+//Make lists buttons drop down the lists
+var dropdown = document.getElementsByClassName('dropBtn');
+var i;
 
-//Global variable
-const hotelInfoHTML = document.getElementById('hotelInfo')
-const hotelDataHTML = document.getElementById('hotelData')
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
 
-//Event listener
-document.getElementById('hotelBtn').addEventListener('click', hotelInfo)
+//Make task lists
 
+//Packing list
+var taskInput = document.getElementById('new-task')
+var addButton = document.getElementById('packAddBtn')
+var incompleteTasksHolder = document.getElementById('incomplete-tasks')
+var completedTasksHolder = document.getElementById('completed-tasks')
 
-//Event for add hotel form
-function hotelInfo(event) {
-    event.prefentDefault()
+//New list item
+var createNewTaskElement = function(taskString) {
+    //Create list items
+    var listItem = document.createElement('li')
+    var checkBox = document.createElement('input')
+    var label = document.createElement('label')
+    var editInput = document.createElement('input')
+    var editButton = document.createElement('button')
+    var deleteButton = document.createElement('button')
 
-    const hForm = hotelInfoHTML.createElement('form')
-    const hNameLabel = hForm.createElement('label')
-    const hNameInput = hForm.createElement('input')
-    const hAddressLabel = hForm.createElement('label')
-    const hAddressInput = hForm.createElement('input')
-    const hCheckInLabel = hForm.createElement('label')
-    const hCheckInDate =  hForm.createElement('input')
-    const hCheckInTime = hForm.createElement('input')
-    const hCheckOutLabel = hForm.createElement('label')
-    const hCheckOutDate = hForm.createElement('input')
-    const hCheckOutTime = hForm.createElement('input')
-    const hExtraInfo = hForm.createElement('input')
-    const hSubmit = hForm.createElement('button')
+    //Modifying the created elements
+    checkBox.type = "checkbox"
 
-    hForm.setAttribute('id', 'hForm')
-    hNameLabel.setAttribute('for', 'hName')
-    hAddressLabel.setAttribute('for', 'hAddress')
-    hCheckInLabel.setAttribute('for', 'checkIn')
-    hCheckOutLabel.setAttribute('for', 'checkOut')
+    deleteButton.innerText = "X"
+    deleteButton.className = "delete"
 
-    objectAssign(hNameInput, {
-        type: 'text',
-        id: 'hName',
-        placeholder: 'Hotel name'
-    })
-    objectAssign(hAddressInput, {
-        type: 'text',
-        id: 'hAddress',
-        placeholder: 'Hotel address'
-    })
-    objectAssign(hCheckInDate, {
-        type: 'date',
-        id: 'hCheckInDate',
-    })
-    objectAssign(hCheckInTime, {
-        type: 'time',
-        id: 'hCheckInTime',
-    })
-    objectAssign(hCheckOutDate, {
-        type: 'date',
-        id: 'hCheckOutDate',
-    })
-    objectAssign(hCheckOutTime, {
-        type: 'time',
-        id: 'hCheckOutTime',
-    })
-    objectAssign(hExtraInfo, {
-        type: 'text',
-        id: 'hExtraInfo',
-        placeholder: 'Add extra information'
-    })
-    objectAssign(hSubmit, {
-        type: 'submit',
-        id: 'hSubmit',
-        value: 'Submit'
-    })
+    label.innerText = taskString
+
+    //Appending
+    listItem.appendChild(checkBox)
+    listItem.appendChild(label)
+    listItem.appendChild(deleteButton)
+
+    return listItem
+}
+
+//Add new task
+var addTask = function() {
+    console.log('Add item..')
+    var listItem = createNewTaskElement(taskInput.value)
+    incompleteTasksHolder.appendChild(listItem)
+    bindTaskEvents(listItem, taskCompleted)
+
+    taskInput.value = ""
+}
+
+//Delete existing task
+var deleteTask = function() {
+    console.log('Delete item..')
+    var listItem = this.parentNode
+    var ul = listItem.parentNode
+
+    ul.removeChild(listItem)
+}
+
+//Mark task complete
+var taskCompleted = function() {
+    console.log('Task complete..')
+    //Append task list item to #completed-tasks
+    var listItem = this.parentNode
+    completedTasksHolder.appendChild(listItem)
+    bindTaskEvents(listItem, taskIncomplete)
+}
+
+//Mark task incomplete
+var taskIncomplete = function() {
+    console.log('Task incomplete')
+    //Append list item to #incomplete-tasks
+    var listItem = this.parentNode
+    incompleteTasksHolder.appendChild(listItem)
+    bindTaskEvents(listItem, taskCompleted)
+}
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+    console.log('Bind list item events')
+    var checkBox = taskListItem.querySelector('input[type=checkbox]')
+    var deleteButton = taskListItem.querySelector('button.delete')
     
+    deleteButton.onclick = deleteTask
+    checkBox.onchange = checkBoxEventHandler
+}
+
+//Set the clickhandler to addTask
+addButton.addEventListener('click', addTask)
+
+//Cycle over incompleteTasksHolder ul list items
+for(var i = 0; i < incompleteTasksHolder.children.length; i++) {
+    bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted)
+}
+
+//Cycle over completedTasksHolder ul list items
+for(var i = 0; i < completedTasksHolder.children.length; i++) {
+    bindTaskEvents(completedTasksHolder.children[i], taskIncomplete)
 }
