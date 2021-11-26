d76b2b857b3a7a26b0a6a0b9ba411bea8a3d591a
vildsee <78222196+vildsee@users.noreply.github.com>
2021-11-26 15:28:40 +0100

    Update countdownTest.test.js

--- __test__/countdownTest.test.js
+++ __test__/countdownTest.test.js
@@ -1,15 +1,4 @@
 import { countdownTest } from "../src/client/js/tests.js";
-// function countdownTest () {
-    
-//     const day = new Date().getTime()
-//     const startDate = new Date(document.getElementById('start').value)
-
-//     count = startDate - day
-//     var days = Math.floor(count / (1000 * 60 * 60 * 24));
-    
-//     return days
-// };
-
 
 describe('test date coundown', () => {
     test('testing', () => {
