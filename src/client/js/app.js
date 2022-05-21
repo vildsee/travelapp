d1b9b710d5f4b78c3ba98cbc32506d915d68d9ee
vildsee <78222196+vildsee@users.noreply.github.com>
2022-05-21 19:22:51 +0200

    weatherbit key, env

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,73 +1,69 @@
 /* GeoNames API Variables */
-const geoBaseUrl = 'https://secure.geonames.org/searchJSON?q=';
-const username = '&username=vildsee';
+const geoBaseUrl = "https://secure.geonames.org/searchJSON?q=";
+const username = "&username=vildsee";
 
 /* Weatherbit API Variables */
-const weatherBaseUrl = 'https://api.weatherbit.io/v2.0/forecast/daily?';
-const weatherKey = '&key=1a77645c2dca40088908444918bacbb7';
+const weatherBaseUrl = "https://api.weatherbit.io/v2.0/forecast/daily?";
+const weatherKey = "&key=" + process.env.API_KEY_WEATHERBIT;
 
 /* Pixabay API Variables */
-const pixabayBaseUrl = 'https://pixabay.com/api/';
-const pixabayKey = '?key=24291413-a1c99d0fb3b3a66d3cf315472';
+const pixabayBaseUrl = "https://pixabay.com/api/";
+const pixabayKey = "?key=" + process.env.API_KEY_PIXABAY;
 
 // Create a new date instance dynamically with JS
 const day = new Date();
-let today = day.getDate() + '.' + (day.getMonth() +1) + '.' + day.getUTCFullYear();
+let today =
+  day.getDate() + "." + (day.getMonth() + 1) + "." + day.getUTCFullYear();
 let forecastDate = new Date(day.setDate(day.getDate() + 15));
-document.getElementById('date').innerHTML = today;
+document.getElementById("date").innerHTML = today;
 
 //Clock
 function startTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
-    setTimeout(startTime, 1000);
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i}  // add zero in front of numbers < 10
-    return i;
-  }
-window.addEventListener('load', startTime())
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+window.addEventListener("load", startTime());
 
 //Global variables
-const wHeader = document.getElementById('wHeader')
-const icon = document.getElementById('weatherIcon')
-const forecast = document.getElementById('forecast')
-const fHr = document.getElementById('fHr')
+const wHeader = document.getElementById("wHeader");
+const icon = document.getElementById("weatherIcon");
+const forecast = document.getElementById("forecast");
+const fHr = document.getElementById("fHr");
 
 //Eventlistener for id=generate
-document.querySelector('#submitTrip').addEventListener('click', performAction);
+document.querySelector("#submitTrip").addEventListener("click", performAction);
 
 //Event
 function performAction(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const city = document.getElementById('inputLocation').value;
-    const day = new Date().getTime()
-    const startDate = new Date(document.getElementById('start').value)
+  const city = document.getElementById("inputLocation").value;
+  const day = new Date().getTime();
+  const startDate = new Date(document.getElementById("start").value);
 
-    count = startDate - day
+  count = startDate - day;
 
-    getCoordinates(geoBaseUrl, city, username)
+  getCoordinates(geoBaseUrl, city, username)
     .then(async (geodata) => {
-
-        const lat = geodata.geonames[0].lat;
-        const lng = geodata.geonames[0].lng;
-        const res = await
-
-        getWeather(weatherBaseUrl, lat, lng, weatherKey)
-        .then(async (weatherData) => {
-            const res = await
-
-            getPics(pixabayBaseUrl, pixabayKey, city)
-            .then(async (picData) => {
-            const res = await                
-            
-            postData('/all', {
+      const lat = geodata.geonames[0].lat;
+      const lng = geodata.geonames[0].lng;
+      const res = await getWeather(weatherBaseUrl, lat, lng, weatherKey).then(
+        async (weatherData) => {
+          const res = await getPics(pixabayBaseUrl, pixabayKey, city).then(
+            async (picData) => {
+              const res = await postData("/all", {
                 city: city,
                 lat: geodata.geonames[0].lat,
                 lng: geodata.geonames[0].lng,
@@ -87,130 +83,134 @@ function performAction(event) {
                 fut_max_temp: weatherData.data[15].max_temp,
                 futWind: weatherData.data[15].wind_spd,
                 futIcon: weatherData.data[15].weather.icon,
-                                
-                })
-            })
-        })                
-        
-        countdown()
-        tripLength()
+              });
+            }
+          );
+        }
+      );
+
+      countdown();
+      tripLength();
     })
-    
+
     .then(() => updateUI());
 }
 
-
-
 //Async POST
-const postData = async (url='', data={}) => {
-    const res = await fetch('http://localhost:6061/add', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-        console.log(res, 'res')
-        console.log(res.body, 'res body')
-    try {
-        const newData = await res.json();
-        return newData;
-    }  catch(error) {
-        console.log('error async post', error);
-    }
+const postData = async (url = "", data = {}) => {
+  const res = await fetch("http://localhost:6061/add", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  console.log(res, "res");
+  console.log(res.body, "res body");
+  try {
+    const newData = await res.json();
+    return newData;
+  } catch (error) {
+    console.log("error async post", error);
+  }
 };
 
 // Async GET
 const getData = async () => {
-    const req = await fetch('http://localhost:6061/add');
-    try {
-        let geodata = await req.json()
-        console.log(geodata);
-        return geodata;
-    } catch(error) {
-        console.log('error async get', error);
-    }
+  const req = await fetch("http://localhost:6061/add");
+  try {
+    let geodata = await req.json();
+    console.log(geodata);
+    return geodata;
+  } catch (error) {
+    console.log("error async get", error);
+  }
 };
 
 //Request to GeoNames API
-const getCoordinates = async(geoBaseUrl, city, username) => {
-    const res = await fetch(geoBaseUrl+city+'&maxRows=1'+username)
-    console.log('url', res)
-    try {
-        const geodata = await res.json();
-        console.log(geodata.geonames[0])
-        console.log(`Lat: ${geodata.geonames[0].lat}`)
-        console.log(`Lng: ${geodata.geonames[0].lng}`)
-        console.log(`CountryName: ${geodata.geonames[0].countryName}`)
-        return geodata
-    } catch(error) {
-        console.log('error getCoordinates');
-    }
+const getCoordinates = async (geoBaseUrl, city, username) => {
+  const res = await fetch(geoBaseUrl + city + "&maxRows=1" + username);
+  console.log("url", res);
+  try {
+    const geodata = await res.json();
+    console.log(geodata.geonames[0]);
+    console.log(`Lat: ${geodata.geonames[0].lat}`);
+    console.log(`Lng: ${geodata.geonames[0].lng}`);
+    console.log(`CountryName: ${geodata.geonames[0].countryName}`);
+    return geodata;
+  } catch (error) {
+    console.log("error getCoordinates");
+  }
 };
 
 //Request to Weatherbit
 const getWeather = async (weatherBaseUrl, lat, lng, weatherKey) => {
-    const res = await fetch(weatherBaseUrl + '&lat=' + lat + '&lon=' + lng + weatherKey)
-    console.log('url', res)
-    try {
-        const weatherData = await res.json();
-        console.log(weatherData)
-        return weatherData;
-    } catch(error) {
-        console.log('error req to weatherbit', error)
-    }
-}
+  const res = await fetch(
+    weatherBaseUrl + "&lat=" + lat + "&lon=" + lng + weatherKey
+  );
+  console.log("url", res);
+  try {
+    const weatherData = await res.json();
+    console.log(weatherData);
+    return weatherData;
+  } catch (error) {
+    console.log("error req to weatherbit", error);
+  }
+};
 
 //Request to Pixabay
 const getPics = async (pixabayBaseUrl, pixabayKey, city) => {
-    const res = await fetch(pixabayBaseUrl + pixabayKey + '&q=' + city + '&image_type=photo')
-    console.log('url', res)
-    try {
-        const picData = await res.json();
-        console.log(picData);
-        console.log(picData.hits[0].webformatURL)
-        return picData;
-    } catch(error) {
-        console.log('error req to pixabay', error)
-    }
-}
+  const res = await fetch(
+    pixabayBaseUrl + pixabayKey + "&q=" + city + "&image_type=photo"
+  );
+  console.log("url", res);
+  try {
+    const picData = await res.json();
+    console.log(picData);
+    console.log(picData.hits[0].webformatURL);
+    return picData;
+  } catch (error) {
+    console.log("error req to pixabay", error);
+  }
+};
 
 //Update UI
 const updateUI = async () => {
-    const req = await fetch('http://localhost:6061/all');
-    try {
-        const projectData = await req.json();
-        const pic = projectData.citypic;
+  const req = await fetch("http://localhost:6061/all");
+  try {
+    const projectData = await req.json();
+    const pic = projectData.citypic;
 
-        countdownConditions()
+    countdownConditions();
 
-        document.getElementById('destination').innerHTML = `${projectData.name}, ${projectData.countryName}`;
-        document.getElementById('cityImage').innerHTML = `<img src="${pic}" alt="img not found" width="200px"/><hr>`;
-        document.getElementById('duration').innerHTML = `${startDate.toDateString()}-${endDate.toDateString()}`;
+    document.getElementById(
+      "destination"
+    ).innerHTML = `${projectData.name}, ${projectData.countryName}`;
+    document.getElementById(
+      "cityImage"
+    ).innerHTML = `<img src="${pic}" alt="img not found" width="200px"/><hr>`;
+    document.getElementById(
+      "duration"
+    ).innerHTML = `${startDate.toDateString()}-${endDate.toDateString()}`;
 
-        if (countdown() <= 6){
-                wHeader.innerHTML = `Weather now:`;
-                icon.innerHTML = `<img src="https://www.weatherbit.io/static/img/icons/${projectData.todayIcon}.png" alt="weather icon"/>`;
-                forecast.innerHTML = `${projectData.description}<br>Temp is ${projectData.temp}°C, wind speed is ${projectData.todayWind}m/s`;
-                fHr.innerHTML = `<hr>`;
-
-            } 
-            else if (countdown() <= 15){
-                wHeader.innerHTML = `Weather on ${startDate.toDateString()}:`;
-                icon.innerHTML = `<img src="https://www.weatherbit.io/static/img/icons/${projectData.weekIcon}.png" alt="weather icon"/>`;
-                forecast.innerHTML = `Temperature will be between ${projectData.week_min_temp}°C and ${projectData.week_max_temp}°C, wind ${projectData.weekWind}m/s`;
-                fHr.innerHTML = `<hr>`;
-            } 
-            else {
-                wHeader.innerHTML = `Weather on ${forecastDate.toDateString()}`;
-                icon.innerHTML = `<img src="https://www.weatherbit.io/static/img/icons/${projectData.futIcon}.png" alt="weather icon"/>`;
-                forecast.innerHTML = `Temperature will be between ${projectData.fut_min_temp}°C and ${projectData.fut_max_temp}°C, wind ${projectData.futWind}m/s`;
-                fHr.innerHTML = `<hr>`;
-            }
-        
-            
-    } catch(error){
-        console.log('error UI', error);
+    if (countdown() <= 6) {
+      wHeader.innerHTML = `Weather now:`;
+      icon.innerHTML = `<img src="https://www.weatherbit.io/static/img/icons/${projectData.todayIcon}.png" alt="weather icon"/>`;
+      forecast.innerHTML = `${projectData.description}<br>Temp is ${projectData.temp}°C, wind speed is ${projectData.todayWind}m/s`;
+      fHr.innerHTML = `<hr>`;
+    } else if (countdown() <= 15) {
+      wHeader.innerHTML = `Weather on ${startDate.toDateString()}:`;
+      icon.innerHTML = `<img src="https://www.weatherbit.io/static/img/icons/${projectData.weekIcon}.png" alt="weather icon"/>`;
+      forecast.innerHTML = `Temperature will be between ${projectData.week_min_temp}°C and ${projectData.week_max_temp}°C, wind ${projectData.weekWind}m/s`;
+      fHr.innerHTML = `<hr>`;
+    } else {
+      wHeader.innerHTML = `Weather on ${forecastDate.toDateString()}`;
+      icon.innerHTML = `<img src="https://www.weatherbit.io/static/img/icons/${projectData.futIcon}.png" alt="weather icon"/>`;
+      forecast.innerHTML = `Temperature will be between ${projectData.fut_min_temp}°C and ${projectData.fut_max_temp}°C, wind ${projectData.futWind}m/s`;
+      fHr.innerHTML = `<hr>`;
     }
+  } catch (error) {
+    console.log("error UI", error);
+  }
 };
